6f07dda33e1d34a06cb0c33370810b854a12926c
Kasketto001 <kaskettomaledetto@gmail.com>
2024-05-30 16:50:32 +0200

    Add ProjectController for API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\ProjectController;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('projects', [ProjectController::class, 'index']);
+
+
+
+// Route::get('project',function(){
+    //     $projects = Project::with('technologies', 'type')->paginate(6);
+    //     return response()->json([
+    //         'success' => true,
+    //         'projects' => $projects
+    //     ]);
+    // });
